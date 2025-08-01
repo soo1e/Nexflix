baf18137739a1ed1234a4ee7c954f2ba87c097cf
soo1e <gameswuon@gmail.com>
2025-08-01 17:22:04 +0900

    컨트롤러 첫 요청 보내고 API 틀 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Movie {
+  id: number;
+  title: string;
+}
+
+@Controller('movie')
 export class AppController {
+  private movies: Movie[] = [
+    { id: 1, title: '해리 포터' },
+    { id: 2, title: '반지의 제왕' },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return this.movies;
+  }
+  
+  @Get(':id')
+  getMovie() {
+    return {
+      id: 1,
+      name: '해리 포터',
+      character: ['해리 포터', '엠마 왓슨'],
+    };
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '캡틴 아메리카'],
+    };
+  }
+
+  @Patch(':id')
+  patchMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '블랙 위도우'],
+    };
+  }
+
+  @Delete(':id')
+  deleteMovie() {
+    return 3;
   }
 }
